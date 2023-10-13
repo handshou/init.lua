0b7ca2b894bbd675ee29686d5391767a2c8bb53b
hansel <handshou@users.noreply.github.com>
2023-10-13 18:45:04 +0800

    feat: add leetcode.nvim

--- lua/h/lazy.lua
+++ lua/h/lazy.lua
@@ -80,6 +80,25 @@ local plugin = {
           {'MunifTanjim/nui.nvim'},
       },
   },
+  {
+      "kawre/leetcode.nvim",
+      build = ":TSUpdate html",
+      dependencies = {
+          "nvim-treesitter/nvim-treesitter",
+          "nvim-telescope/telescope.nvim",
+          "nvim-lua/plenary.nvim", -- required by telescope
+          "MunifTanjim/nui.nvim",
+
+          -- optional
+          "nvim-tree/nvim-web-devicons",
+
+          -- recommended
+          -- "rcarriga/nvim-notify",
+      },
+      opts = {
+          -- configuration goes here
+      },
+  }
 }
 
 require("lazy").setup(plugin, {})
