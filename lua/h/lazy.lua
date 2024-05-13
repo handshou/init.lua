0346508f2128760a06deb717422b60f0767ab3b9
hansel <handshou@users.noreply.github.com>
2024-05-13 14:13:40 +0800

    fix: update telescope

--- lua/h/lazy.lua
+++ lua/h/lazy.lua
@@ -15,7 +15,7 @@ vim.g.mapleader = ' '
 
 local plugin = {
    {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
 	  -- or                            , branch = '0.1.x',
 	  dependencies = { {'nvim-lua/plenary.nvim'} }
   },
