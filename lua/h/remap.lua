0e1014ca3e61f50e7491e23cfd978235ec779c23
hansel <handshou@users.noreply.github.com>
2024-11-30 01:25:10 +0800

    feat: biome format

--- lua/h/remap.lua
+++ lua/h/remap.lua
@@ -10,15 +10,30 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>f", function()
+    vim.lsp.buf.format({
+        filter = function(client)
+            return client.name == "biome"
+        end,
+        async = true,
+    })
+end, { desc = "Format with Biome" })
+vim.keymap.set("n", "<leader>F", function()
+    require("conform").format({
+        lsp_fallback = true, -- Use LSP formatting if no conform formatter is configured
+    })
+end, { desc = "Format with Conform" })
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end)
+vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end)
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
