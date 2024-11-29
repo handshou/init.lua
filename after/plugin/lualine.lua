5f0853b57e87f0059c0ab9e4f67c1cad11d2a195
hansel <handshou@users.noreply.github.com>
2024-11-30 01:25:59 +0800

    style: lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,65 +1,29 @@
 vim.cmd [[highlight WinSeparator guifg=#4e545c guibg=None]]
 require('lualine').setup {
   options = {
+    theme = "tokyonight",
     icons_enabled = true,
-    theme = 'tokyonight',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
     globalstatus = true,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "█", right = "█" },
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_b = {
+      "diagnostics",
+    },
     lualine_c = {
       {
         'filename',
         file_status = true,
-        path = 2,
-        shorting_target = 55,
+        path = 4,
+        shorting_target = 90,
         symbols = {
           unnamed = ''
         }
       }
     },
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_x = {
+      "filetype",
+    },
   },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  inactive_winbar = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  extensions = {}
 }
