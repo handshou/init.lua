4b728b06a2f236a8c223be9d9c9b1bf0449264af
hansel <handshou@users.noreply.github.com>
2024-11-30 01:24:40 +0800

    chore: lint

--- lua/h/lazy.lua
+++ lua/h/lazy.lua
@@ -1,38 +1,39 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 local plugin = {
-  "stevearc/conform.nvim",
+    "stevearc/conform.nvim",
     opts = {
-       formatters_by_ft = {
-        lua = { "stylua" },
-        ["javascript"] = { "biome-check" },
-        ["javascriptreact"] = { "biome-check" },
-        ["typescript"] = { "biome-check" },
-        ["typescriptreact"] = { "biome-check" },
-        ["json"] = { "biome-check" },
-        ["css"] = { "biome-check" },
-      },
+        formatters_by_ft = {
+            lua = { "stylua" },
+            ["astro"] = { "biome-check" },
+            ["javascript"] = { "biome-check" },
+            ["javascriptreact"] = { "biome-check" },
+            ["typescript"] = { "biome-check" },
+            ["typescriptreact"] = { "biome-check" },
+            ["json"] = { "biome-check" },
+            ["css"] = { "biome-check" },
+        },
     },
     {
         "ThePrimeagen/harpoon",
         branch = "harpoon2",
         dependencies = {
-          { "nvim-lua/plenary.nvim" },
-          { "nvim-telescope/telescope.nvim" },
-        }
+            { "nvim-lua/plenary.nvim" },
+            { "nvim-telescope/telescope.nvim" },
+        },
     },
     {
         "christoomey/vim-tmux-navigator",
@@ -44,144 +45,146 @@ local plugin = {
             "TmuxNavigatePrevious",
         },
         keys = {
-            { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-            { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-            { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-            { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+            { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
             { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
         },
         lazy = false,
     },
-   {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
-	  -- or                            , branch = '0.1.x',
-	  dependencies = { {'nvim-lua/plenary.nvim'} }
-  },
-
-  {
-      "folke/tokyonight.nvim",
-      lazy = false,
-      priority = 1000,
-      config = function()
-        -- load the colorscheme here
-        vim.cmd([[colorscheme tokyonight]])
-      end,
-  },
-
-  'nvim-treesitter/nvim-treesitter', build = ':TSUpdate',
-  'nvim-treesitter/playground',
-  'mbbill/undotree',
-  'tpope/vim-fugitive',
-  "nvim-treesitter/nvim-treesitter-context",
-
-   {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
-	  dependencies = {
-		  -- LSP Support
-		  {'williamboman/mason.nvim'},
-	  {'williamboman/mason-lspconfig.nvim'}, -- Optional
-	  {'neovim/nvim-lspconfig'},             -- Required
-
-	  -- Autocompletion
-	  {'hrsh7th/nvim-cmp'},         -- Required
-	  {'hrsh7th/cmp-nvim-lsp'},     -- Required
-	  {'hrsh7th/cmp-buffer'},       -- Optional
-	  {'hrsh7th/cmp-path'},         -- Optional
-	  {'saadparwaiz1/cmp_luasnip'}, -- Optional
-	  {'hrsh7th/cmp-nvim-lua'},     -- Optional
-
-	  -- Snippets
-      {'L3MON4D3/LuaSnip'},             -- Required
-	  {'rafamadriz/friendly-snippets'}, -- Optional
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.4",
+        -- or                            , branch = '0.1.x',
+        dependencies = { { "nvim-lua/plenary.nvim" } },
     },
 
-     config = function()
-      -- Configure Mason and LSP handlers
-      require("mason").setup()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "biome" },
-      })
-
-      require("mason-lspconfig").setup_handlers({
-        ["biome"] = function()
-          require("lspconfig").biome.setup({
-            cmd = { "biome", "lsp-proxy" },
-            filetypes = { "javascript", "typescript", "json", "css", "scss", "html", "markdown", "yaml" },
-            root_dir = require("lspconfig.util").root_pattern(".biomeconfig.json", "package.json"),
-          })
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            -- load the colorscheme here
+            vim.cmd([[colorscheme tokyonight]])
         end,
-      })
-    end,
-  },
+    },
 
-  'folke/zen-mode.nvim',
-  'laytan/cloak.nvim',
-  {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      dependencies = "nvim-treesitter/nvim-treesitter",
-  },
-  {
-      'nvim-lualine/lualine.nvim',
-      dependencies = { 'nvim-tree/nvim-web-devicons' },
-  },
-  {
-      'VonHeikemen/fine-cmdline.nvim',
-      dependencies = {
-          {'MunifTanjim/nui.nvim'},
-      },
-  },
-  {
-      "davidmh/mdx.nvim",
-      config = true,
-      dependencies = {"nvim-treesitter/nvim-treesitter"}
-  },
-  {
-      "kawre/leetcode.nvim",
-      build = ":TSUpdate html",
-      dependencies = {
-          "nvim-treesitter/nvim-treesitter",
-          "nvim-telescope/telescope.nvim",
-          "nvim-lua/plenary.nvim", -- required by telescope
-          "MunifTanjim/nui.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    "nvim-treesitter/playground",
+    "mbbill/undotree",
+    "tpope/vim-fugitive",
+    "nvim-treesitter/nvim-treesitter-context",
 
-          -- optional
-          "nvim-tree/nvim-web-devicons",
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v1.x",
+        dependencies = {
+            -- LSP Support
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" }, -- Optional
+            { "neovim/nvim-lspconfig" },       -- Required
 
-          -- recommended
-          -- "rcarriga/nvim-notify",
-      },
-      opts = {
-          domain = "com", -- For now "com" is the only one supported
+            -- Autocompletion
+            { "hrsh7th/nvim-cmp" },   -- Required
+            { "hrsh7th/cmp-nvim-lsp" }, -- Required
+            { "hrsh7th/cmp-buffer" }, -- Optional
+            { "hrsh7th/cmp-path" },   -- Optional
+            { "saadparwaiz1/cmp_luasnip" }, -- Optional
+            { "hrsh7th/cmp-nvim-lua" }, -- Optional
 
-          arg = "leetcode.nvim",
+            -- Snippets
+            { "L3MON4D3/LuaSnip" },       -- Required
+            { "rafamadriz/friendly-snippets" }, -- Optional
+        },
 
-          lang = "javascript",
+        config = function()
+            -- Configure Mason and LSP handlers
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "biome" },
+            })
 
-          sql = "postgresql",
+            require("mason-lspconfig").setup_handlers({
+                ["biome"] = function()
+                    require("lspconfig").biome.setup({
+                        cmd = { "biome", "lsp-proxy" },
+                        filetypes = { "javascript", "typescript", "json", "css", "scss", "html", "markdown", "yaml" },
+                        root_dir = require("lspconfig.util").root_pattern(".biomeconfig.json", "package.json"),
+                    })
+                end,
+            })
+        end,
+    },
 
-          ---@type string
-          directory = vim.fn.stdpath("data") .. "/leetcode/",
+    "folke/zen-mode.nvim",
+    "laytan/cloak.nvim",
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+    {
+        "VonHeikemen/fine-cmdline.nvim",
+        dependencies = {
+            { "MunifTanjim/nui.nvim" },
+        },
+    },
+    {
+        "davidmh/mdx.nvim",
+        config = true,
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+    },
+    {
+        "kawre/leetcode.nvim",
+        build = ":TSUpdate html",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-telescope/telescope.nvim",
+            "nvim-lua/plenary.nvim", -- required by telescope
+            "MunifTanjim/nui.nvim",
 
-          ---@type boolean
-          logging = true,
+            -- optional
+            "nvim-tree/nvim-web-devicons",
 
-          console = {
-              ---@type boolean
-              open_on_runcode = false,
+            -- recommended
+            -- "rcarriga/nvim-notify",
+        },
+        opts = {
+            domain = "com", -- For now "com" is the only one supported
 
-              size = {
-                  width = "75%", ---@type string | integer
-                  height = "75%", ---@type string | integer
-              },
-              dir = "row", ---@type "col" | "row"
-          },
+            arg = "leetcode.nvim",
 
-          description = {
-              width = "40%", ---@type string | integer
-          },
-      },
-  }
+            lang = "javascript",
+
+            sql = "postgresql",
+
+            ---@type string
+            directory = vim.fn.stdpath("data") .. "/leetcode/",
+
+            ---@type boolean
+            logging = true,
+
+            console = {
+                ---@type boolean
+                open_on_runcode = false,
+
+                size = {
+                    width = "75%", ---@type string | integer
+                    height = "75%", ---@type string | integer
+                },
+                dir = "row", ---@type "col" | "row"
+            },
+
+            description = {
+                width = "40%", ---@type string | integer
+            },
+        },
+    },
 }
 
 require("lazy").setup(plugin, {})
@@ -220,4 +223,3 @@ require("tokyonight").setup({
 })
 
 vim.cmd.colorscheme("tokyonight")
-
