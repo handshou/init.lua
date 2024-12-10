c56e6e3583f5d9733c7f11d6dc28005132ae93f1
hansel <handshou@users.noreply.github.com>
2024-12-10 10:39:44 +0800

    feat(telescope): theme, use fzf-native

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,11 @@
+require('telescope').setup({
+    pickers = {
+        find_files = {
+            theme = "ivy"
+        }
+    }
+})
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pF', function()
--- lua/h/lazy.lua
+++ lua/h/lazy.lua
@@ -99,7 +99,7 @@ local plugin = {
         "nvim-telescope/telescope.nvim",
         -- tag = "0.1.4",
         -- or                            , branch = '0.1.x',
-        dependencies = { { "nvim-lua/plenary.nvim" } },
+        dependencies = { { "nvim-lua/plenary.nvim" }, { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' } },
     },
 
     {
