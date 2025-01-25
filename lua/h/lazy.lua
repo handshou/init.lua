71847b3def26801d875ef1d62f1ee992e4b528b6
hansel <handshou@users.noreply.github.com>
2025-01-25 23:40:29 +0800

    feat: trouble, quickfix and location lists

--- lua/h/lazy.lua
+++ lua/h/lazy.lua
@@ -15,6 +15,45 @@ vim.g.mapleader = " "
 
 local plugin = {
     {
+        "folke/trouble.nvim",
+        opts = {
+            use_diagnostic_signs = true,
+        },
+        cmd = "Trouble",
+        keys = {
+            {
+                "<leader>tt",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            {
+                "<leader>tT",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<leader>cs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<leader>cl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
+            },
+            {
+                "<leader>xL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)",
+            },
+            {
+                "<leader>xQ",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)",
+            },
+        },
+    },
+    {
         "nvim-telescope/telescope-media-files.nvim"
     },
     {
--- lua/h/remap.lua
+++ lua/h/remap.lua
@@ -5,6 +5,8 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<PageDown>", "<C-d>zz")
+vim.keymap.set("n", "<PageUp>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
@@ -30,6 +32,11 @@ vim.keymap.set("n", "<leader>F", function()
         lsp_fallback = true, -- Use LSP formatting if no conform formatter is configured
     })
 end, { desc = "Format with Conform" })
+
+vim.keymap.set("n", "\u{AA}", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "\u{AB}", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
