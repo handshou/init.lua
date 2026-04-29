3b9c9344b258408f2b44e5b7fdc47ad49cb59d12
hansel <handshou@users.noreply.github.com>
2026-04-29 10:16:38 +0800

    disable flake8 diagnostics

--- lua/h/lazy.lua
+++ lua/h/lazy.lua
@@ -28,7 +28,7 @@ local plugin = {
             local null_ls = require("null-ls")
             null_ls.setup({
                 sources = {
-                    null_ls.builtins.diagnostics.flake8,
+                    -- null_ls.builtins.diagnostics.flake8,
                 },
             })
         end
